26a6c48b8ff80941998c7bed1c50ae927d12c6d7
cshea15 <chashea@microsoft.com>
2024-05-30 15:27:24 -0400

    updating

--- outputs.tf
+++ outputs.tf
@@ -17,5 +17,5 @@ EOT
 
 output "resource_id" {
   description = "This is the full output for the Log Analytics resource ID. This is the default output for the module following AVM standards. Review the examples below for the correct output to use in your module."
-  value       = module.log_analytics.resource_id
+  value       = azurerm_log_analytics_workspace.this.id
 }
