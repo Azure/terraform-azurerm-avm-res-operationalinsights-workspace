4262252c2e5a80bcc199b810f8fddaf3e0ec654d
Charles J Shea <58995422+cshea-msft@users.noreply.github.com>
2024-08-02 12:22:17 -0400

    v0.3.3  (#76)
    * added sensitive tag
    
    * update output

--- outputs.tf
+++ outputs.tf
@@ -12,6 +12,7 @@ output "resource" {
   - module.log_analytics.resource.id
   - module.log_analytics.resource.name
 EOT
+  sensitive   = true
   value       = azurerm_log_analytics_workspace.this
 }
 
