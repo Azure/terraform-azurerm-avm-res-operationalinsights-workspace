915196ca6e9ad133b6feff71ed3a8aa805555efb
rbradley51 <rbradley51@gmail.com>
2024-08-22 17:41:00 +0000

    post pr-check update

--- main.ampls_connect.tf
+++ main.ampls_connect.tf
@@ -1,15 +1,13 @@
 resource "azapi_resource" "ampls" {
-  type     = "Microsoft.Insights/privateLinkScopes/scopedResources@2021-07-01-preview"
   for_each = var.monitor_private_link_scoped_resource
 
-  name          = each.value.name != null ? each.value.name : azurerm_log_analytics_workspace.this.name
-  parent_id     = each.value.resource_id
-  ignore_casing = true
-
+  type = "Microsoft.Insights/privateLinkScopes/scopedResources@2021-07-01-preview"
   body = jsonencode({
     properties = {
       linkedResourceId = azurerm_log_analytics_workspace.this.id
     }
   })
-
+  ignore_casing = true
+  name          = each.value.name != null ? each.value.name : azurerm_log_analytics_workspace.this.name
+  parent_id     = each.value.resource_id
 }
